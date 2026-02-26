dfe24c0acfa65f28bb3168209521909b9595c4b6
Ayush Gupta <ayushagupta225@gmail.com>
2026-02-26 14:23:55 +0530

    Fixed errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroSection } from "../components/HeroSection";
 import { Course } from "../components/Course";
 import { AboutSection } from "../components/AboutSection";
 import { StatsSection } from "../components/StatsSection";
+import FAQ from "../components/FAQ"
 import { WhyChooseUs } from "../components/WhyChooseUs";
 import Footer from "../components/Footer";
 
